78b294f63345cf34c25320436e94dc7dfceef2df
Candice Scott-Mason <cscottmason23@gmail.com>
2021-03-31 08:11:41 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,11 +1,55 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue){
+let message;
+if (someValue < 400){
+  message = "This one is on me!"
+}
+
+if (someValue > 400 && someValue < 2500){
+  message = "I will gladly take your thirty bucks."
+}
+
+if (someValue > 2500){
+  message = "No can do."
+}
+  // Write your code here!
+  return message;
+}
+scuberGreetingForFeet(199)
+
+
+
+
+function ternaryCheckCity(someCity){
+  let message;
+
+  someCity === "NYC"? (message = "Ok, sounds good.") : (message = "No go.");
+
+  return message;
+
   // Write your code here!
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(string){
 
-function switchOnCharmFromTip(){
+  let message;
+
+
+  switch(string) {
+    case 'generous':
+      message = 'Thank you so much.';
+      break;
+      
+    case "not as generous":
+      message = "Thank you.";
+
+      break;
+      default:
+      message = "Bye.";
+  }
+
+  return message;
+
+ 
+  
   // Write your code here!
 }
